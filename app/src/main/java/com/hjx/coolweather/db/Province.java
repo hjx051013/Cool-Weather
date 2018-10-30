9c119597647bbddea353bc1f58f81d0424e1f4fc
Jiaxin Huang <jiaxinhuang1013@gmail.com>
2018-10-30 11:41:05 +0800

    加入创建数据库和表的配置

---
+++ app/src/main/java/com/hjx/coolweather/db/Province.java
@@ -0,0 +1,35 @@
+package com.hjx.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private String provinceCode;
+
+}
